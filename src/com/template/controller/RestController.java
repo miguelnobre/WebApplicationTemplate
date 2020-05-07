0a591aef6443f41ad2d923d15b305f5f3adbab4a
Miguel Nobre <miguelnobre@miguelnobre.home>
2020-05-07 04:50:16 +0100

    Add Log4J

--- src/com/template/controller/RestController.java
+++ src/com/template/controller/RestController.java
@@ -1,5 +1,8 @@
 package com.template.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -10,10 +13,13 @@ import javax.ws.rs.core.MediaType;
 @Path("")
 public class RestController {
 
-	@GET
-	@Path("{template}")
-	@Produces(MediaType.TEXT_HTML)
-	public String generateContent(@PathParam("template") String template, @QueryParam("id") String id) {	
-		return "Template: " + template + " | Id: " + id;
-	}
+    private static Logger _log = LoggerFactory.getLogger(RestController.class);
+
+    @GET
+    @Path("{template}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String generateContent(@PathParam("template") String template, @QueryParam("id") String id) {
+        _log.info("[generateContent()] Request Params: Template: {} | Id: {}", template, id);
+        return "Template: " + template + " | Id: " + id;
+    }
 }
